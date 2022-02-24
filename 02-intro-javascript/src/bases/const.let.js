16710c19b84516040dc08df42ddb7062acbcd023
jeisonpimentel <jeisonalvis10@gmail.com>
2022-02-23 22:56:30 -0300

    subiendo cambios

---
+++ 02-intro-javascript/src/bases/const.let.js
@@ -0,0 +1,15 @@
+
+// Variables y constantes
+
+const nombre    = 'Jeison';
+const apellido  = 'Pimentel';
+
+let valorDado   = 6;
+valorDado       = 8;
+
+console.log( nombre, apellido, valorDado );
+
+if ( true ) {
+    const nombre = 'Liah';
+    console.log( nombre );
+} 
